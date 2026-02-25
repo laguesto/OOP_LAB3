f83eb92f3171d400a18f2d6ed73ad1c6e507f9f0
vasiaw <fvlog36@gmail.com>
2026-02-25 16:15:43 +0200

    Lab3_C#

---
+++ Lab3_C#/Lab3_C#/Program.cs
@@ -0,0 +1,16 @@
+﻿using Triangle_class;
+
+Triangle T1 = new Triangle();
+Triangle T2 = new Triangle(2,4.5,6);
+Triangle T3 = new Triangle(T2);
+    
+Console.WriteLine("Triangle 1: " + T1.X + ", " + T1.Y + ", " + T1.Z);
+Console.WriteLine("Triangle 2: " + T2.X + ", " + T2.Y + ", " + T2.Z);
+Console.WriteLine("Triangle 3: " + T3.X + ", " + T3.Y + ", " + T3.Z);
+
+T2 *= 2;
+Console.WriteLine("Triangle 2 after multiply by 2: " + T2.X + ", " + T2.Y + ", " + T2.Z);
+
+T1 = T2 + T3;
+Console.WriteLine("Triangle 1 after summing Triangles 2 and 3: " + T1.X + ", " + T1.Y + ", " + T1.Z);
+
---
+++ Lab3_C#/Triangle_class/Triangle.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangle_class
+{
+    public class Triangle
+    {
+        private double x, y, z;
+
+        public Triangle()
+        {
+            x = 0; y = 0; z = 0;
+        }
+
+        public Triangle(double x, double y, double z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public Triangle(Triangle other_triangle)
+        {
+            x = other_triangle.x;
+            y = other_triangle.y; 
+            z = other_triangle.z;
+        }
+
+
+        public double X { get { return x; } set { x = value; } }
+        public double Y { get { return y; } set { y = value; } }
+        public double Z { get { return z; } set { z = value; } }
+
+       public double Perimeter()
+        {
+            return x + y + z;
+        }
+
+        public double Square()
+        {
+            double p = Perimeter()/2;
+
+            return Math.Sqrt(p * (p - x) * (p - y) * (p - z));
+        }
+
+        public static Triangle operator *(Triangle t, double k)
+        {
+            return new Triangle(t.x * k, t.y * k, t.z * k);
+        }
+
+        public static Triangle operator +(Triangle T1, Triangle T2)
+        {
+            return new Triangle (T1.x + T2.x, T1.y + T2 .y , T1.z + T2.z);
+        }
+    }
+}
